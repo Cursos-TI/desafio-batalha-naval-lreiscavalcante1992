e4592b37613564627da88f7ab3129c6a129bbf75
lreiscavalcante1992 <202502663919@alunos.estacio.br>
2025-04-13 05:19:17 +0000

    finalizei o desafio mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,6 +6,9 @@ int main () {
     int navioVertical[3] = {3, 3, 3};
     int navioD1[3] = {3,3,3};
     int navioD2[3] = {3,3,3};
+    int habilidade_cone [5][5] = {0};
+    int habilidade_cruz [5][5] = {0};
+    int habilidade_octaedro [5][5] = {0};
     int i, j;
     //inicializar tabuleiro com água (0)
 
@@ -17,6 +20,15 @@ int main () {
         }
         
     }
+
+    // Posiciona navios no tabuleiro
+    for ( i = 0; i < 3; i++) {
+        tabuleiro[1][2 + i] = navioHorizontal[i]; //horizontal:linha 2, coluna c-e
+        tabuleiro[4 + i][7] = navioVertical [i]; // vertical: col h, linhas 5-7
+        tabuleiro[i][i] = navioD1[1]; // diagonal principal: (1a,2b,3c)
+        tabuleiro[2 - i][7 + 1] = navioD2 [i]; //diagonal secundaria: (3h, 2i, 3c)
+    }
+    
     // Exibir tabuleiro vazio
     printf("Tabuleiro: \n");
     for ( j = 0; j < 10; j++) {
@@ -26,46 +38,13 @@ int main () {
     for ( i = 0; i < 10; i++)
     {
         if (1 < 9) printf("%d", i + 1);
-        
+        else printf("%d ", i + 1);        
         for (j = 0; j < 10; j++ ) {
-            printf(" %d", tabuleiro[i][j]);
+            printf("0 ");
         }
         printf("\n");
     }
-    
-// coordenadas fixas para os navios
-//navio horizontal de 3 posições começa na linha 2, coluna c (lina=1, coluna = 2)
-
-int linhaH = 1;
-int colunaH = 2;
-
-for ( i = 0; i < 3; i++){
-    tabuleiro[linhaH][colunaH + i] = navioHorizontal [i];
-}
-
-// navio vertical de 3 posições começa em linha 5, coluna 'h' (linha=4, coluna=7)
-
-    int linhaV = 4;
-    int colunaV = 7;
-
-    for ( i = 0; i < 3; i++)
-    {
-        tabuleiro[linhaV + i] [colunaV] = navioVertical[i];
-    }
-     
-    // navio diagonal 1(principal): começa em (0,0) - > (2,2)
-    for ( i = 0; i < 3; i++){
-        tabuleiro[0 + i][0 + i] = navioD1[i];
-        }
-
-        //navio diagonal 2 (secundaria):começa em (2,7) -> (0,9)
-        for ( i = 0; i < 3; i++)
-        {
-            tabuleiro[2 - i][7 + i] = navioD2[i];
-        }
-           
-
-    //Exibe o tabuleiro com os navios
+    //Exibir o tabuleiro com os navios
 
     printf("\nTabuleiro com navios:\n");
     for (j = 0; j < 10; j++) {
@@ -82,9 +61,73 @@ for ( i = 0; i < 3; i++){
         }
         printf("\n");
     }
-     return 0;   
-        
-    }
+
+    //habilidade: cone (centro 2,2)
+    habilidade_cone[2][2] = 5;
+    habilidade_cone[3][1] = 5;
+    habilidade_cone[3][2] = 5;
+    habilidade_cone[3][3] = 5;
+    habilidade_cone[4][0] = 5;
+    habilidade_cone[4][1] = 5;
+    habilidade_cone[4][2] = 5;
+    habilidade_cone[4][3] = 5;
+    habilidade_cone[4][4] = 5;
+
+    //habilidade: cruz (centro 2,2)
+    for ( i = 0; i < 5; i++) {
+        habilidade_cruz [2][i] = 5; // linha do meio
+        habilidade_cruz [i][2] = 5; // coluna meio
+        }
+    
+        // habilidade: octaedro (diamante com centro 2,2)
+        habilidade_octaedro[2][2] = 5;
+        habilidade_octaedro[1][2] = 5;
+        habilidade_octaedro[2][1] = 5;
+        habilidade_octaedro[2][3] = 5;
+        habilidade_octaedro[3][2] = 5;
+        habilidade_octaedro[0][2] = 5;
+        habilidade_octaedro[2][0] = 5;
+        habilidade_octaedro[2][4] = 5;
+        habilidade_octaedro[4][2] = 5;
+
+        // exibir matriz cone
+        printf("\nHabilidade Especial - Cone:\n");
+        for ( i = 0; i < 5; i++)        {
+            for (j = 0; j < 5; j++) {
+                if (habilidade_cone[i][j] == 5)
+                printf("5 ");
+                else
+                printf("0 ");
+                }
+                printf("\n");
+            }
+            // Exibir matriz Cruz
+            printf("\nHabilidade Especial - Cruz:\n");
+            for ( i = 0; i < 5; i++)           {
+                for ( j = 0; j < 5; j++)             {
+                    if (habilidade_cruz[i][j]==5)
+                    printf("5 ");
+                    else
+                    printf("0 ");               
+                }
+                 printf("\n");   
+                }
+                //Exibir matriz Octaedro
+                printf("\nHabilidade Expecial - Octaedro:\n");
+                for ( i = 0; i < 5; i++)             {
+                    for ( j = 0; j < 5; j++)           {
+                        if (habilidade_octaedro [i][j]==5)
+                        printf("5 ");
+                        else
+                        printf("0 ");
+                        }
+                        printf("\n");
+                    }
+                    return 0;
+                }
+                
+            
+  
     
     
 
